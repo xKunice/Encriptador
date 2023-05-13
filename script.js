7c0dee1b55719902721454af3d35dd3d2588b87f
xKunice <132533167+xKunice@users.noreply.github.com>
2023-05-13 03:39:05 -0600

    pagina en 1440 x 1024 con todas las funcionalidades

---
+++ script.js
@@ -0,0 +1,105 @@
+function encriptarPalabra(palabra) {
+    let palabraEncriptada = palabra.replaceAll("e", "enter");
+    palabraEncriptada = palabraEncriptada.replaceAll("i", "imes");
+    palabraEncriptada = palabraEncriptada.replaceAll("a", "ai");
+    palabraEncriptada = palabraEncriptada.replaceAll("o", "ober");
+    palabraEncriptada = palabraEncriptada.replaceAll("u", "ufat");
+
+    return palabraEncriptada;
+}
+
+function desencriptarPalabra(palabraEncriptada) {
+    let palabra = palabraEncriptada.replaceAll("enter", "e");
+    palabra = palabra.replaceAll("imes", "i");
+    palabra = palabra.replaceAll("ai", "a");
+    palabra = palabra.replaceAll("ober", "o");
+    palabra = palabra.replaceAll("ufat", "u");
+
+    return palabra;
+}
+
+function encriptarTexto() {
+
+    let textoIngresado = document.getElementById("textoIngresado").value;
+    let textoMostrado = document.getElementById("textoMostrado");
+    let encriptarPalabras = textoIngresado.split(" ").map(encriptarPalabra);
+
+    textoMostrado.value = encriptarPalabras.join(" ");
+
+}
+
+function desencriptarTexto(){
+
+    let textoIngresado = document.getElementById("textoIngresado").value;
+    let textoMostrado = document.getElementById("textoMostrado");
+    let desencriptarPalabras = textoIngresado.split(" ").map(desencriptarPalabra);
+
+    textoMostrado.value = desencriptarPalabras.join(" ");
+
+}
+
+function copiarTextoMostrado(){
+    const textoMostrado = document.getElementById('textoMostrado');
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(textoMostrado.value)
+        .then(() => {
+          alert('Texto copiado al portapapeles');
+        })
+        .catch((error) => {
+          console.error('Error al copiar el texto: ', error);
+        });
+    } else {
+      // Usar el método antiguo
+      const textarea = document.createElement('textarea');
+      textarea.value = textoMostrado.value;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+      alert('Texto copiado al portapapeles');
+    }
+}
+
+function DesaparecerElementos(){
+    let textoMostrado = document.getElementById('textoMostrado');
+    let elementosMostrados = document.getElementById('elementosMostrados');
+
+    textoMostrado.addEventListener("input", function(){
+        elementosMostrados.style.display = "none";
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const botonEncriptar = document.getElementById("botonEncriptar");
+    botonEncriptar.onclick = encriptarTexto;
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const botonDesencriptar = document.getElementById("botonDesencriptar");
+    botonDesencriptar.onclick = desencriptarTexto;
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const botonCopiar = document.getElementById("botonCopiar");
+    botonCopiar.onclick = copiarTextoMostrado;
+});
+
+document.addEventListener("DOMContentLoaded", function(event){
+    const elementosMostrados = document.getElementById('elementosMostrados');
+    const botonEncriptar = document.getElementById('botonEncriptar');
+
+    botonEncriptar.addEventListener("click", function(){
+        elementosMostrados.style.display = "none";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function(event){
+    const elementosMostrados = document.getElementById('elementosMostrados');
+    const botonDesencriptar = document.getElementById('botonDesencriptar');
+
+    botonDesencriptar.addEventListener("click", function(){
+        elementosMostrados.style.display = "none";
+    });
+});
+ 
